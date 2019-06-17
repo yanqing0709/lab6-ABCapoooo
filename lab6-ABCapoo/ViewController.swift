d5be4425543a7cecede1774ef0847ff981dc426d
陳嬿晴 <smile870709@yahoo.com.tw>
2019-06-17 15:26:43 +0800

    Initial Commit

---
+++ lab6-ABCapoo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab6-ABCapoo
+//
+//  Created by 陳嬿晴 on 2019/6/17.
+//  Copyright © 2019 陳嬿晴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lab6-ABCapooTests/lab6_ABCapooTests.swift
@@ -0,0 +1,34 @@
+//
+//  lab6_ABCapooTests.swift
+//  lab6-ABCapooTests
+//
+//  Created by 陳嬿晴 on 2019/6/17.
+//  Copyright © 2019 陳嬿晴. All rights reserved.
+//
+
+import XCTest
+@testable import lab6_ABCapoo
+
+class lab6_ABCapooTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
